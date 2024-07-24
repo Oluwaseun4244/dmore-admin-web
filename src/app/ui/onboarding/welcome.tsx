ef72172e00499f9694b1239fe2158c76525b0388
mac <mac@macs-MacBook-Pro.local>
2024-07-23 17:06:49 -0700

    staff auth

--- src/app/ui/onboarding/welcome.tsx
+++ src/app/ui/onboarding/welcome.tsx
@@ -11,7 +11,7 @@ import WelcomeDetails from "@/app/components/WelcomeDetails";
 import { OnboardingChildrenProps } from "@/app/utils/definitions";
 
 const Welcome = ({ onNext }: OnboardingChildrenProps) => {
-  const [toggleDetails, setToggleDetails] = useState(false);
+  const [opened, setOpened] = useState("");
 
   const onBuyerSignup = () => {
     onNext("buyer-signup");
@@ -21,53 +21,67 @@ const Welcome = ({ onNext }: OnboardingChildrenProps) => {
     onNext("staff-signup");
   };
 
+  const handleOpened = (value: string) => {
+    if (value === opened) {
+      setOpened("");
+    } else {
+      setOpened(value);
+    }
+  };
+
+
   return (
-    <div className='w-full h-full'>
-      <div className='h-full w-full flex flex-col space-y-10'>
-        <h2 className='font-satoshi font-medium text-black text-[36px] leading-[32px] -tracking-[1%]'>
+    <div className="w-full h-full">
+      <div className="h-full w-full flex flex-col space-y-10">
+        <h2 className="font-satoshi font-medium text-black text-[36px] leading-[32px] -tracking-[1%]">
           Welcome!
         </h2>
-        <div className='flex flex-col space-y-3 hover:translate-y-2 hover:translate-x-2 transition-all duration-300 ease-in-out'>
+        <div className="flex flex-col space-y-3 hover:translate-y-2 hover:translate-x-2 transition-all duration-300 ease-in-out">
           <div
-            className='flex justify-between items-center border border-[#D4C6E8] bg-[#DAB9FA17] bg-opacity-[9%] rounded-[12px] py-3 px-5 cursor-pointer hover:shadow-md hover:shadow-[#8077F626] hover:blur-[4] transition-all duration-300'
-            onClick={() => setToggleDetails(!toggleDetails)}
+            className="flex justify-between items-center border border-[#D4C6E8] bg-[#DAB9FA17] bg-opacity-[9%] rounded-[12px] py-3 px-5 cursor-pointer hover:shadow-md hover:shadow-[#8077F626] hover:blur-[4] transition-all duration-300"
+            onClick={() => handleOpened("buyer")}
           >
-            <div className='flex space-x-1 items-center'>
-              <Image src={create} alt='create' />
-              <p className='font-satoshi font-medium text-[16px] text-[#090B0C] leading-[20px]'>
+            <div className="flex space-x-1 items-center">
+              <Image src={create} alt="create" />
+              <p className="font-satoshi font-medium text-[16px] text-[#090B0C] leading-[20px]">
                 Create an account
               </p>
             </div>
-            {toggleDetails ? (
-              <Image src={arrowDownPurple} alt='arrow down purple' />
+            {opened === "buyer" ? (
+              <Image src={arrowDownPurple} alt="arrow down purple" />
             ) : (
-              <Image src={arrowRightPurple} alt='arrow right purple' />
+              <Image src={arrowRightPurple} alt="arrow right purple" />
             )}
           </div>
-          {toggleDetails && <WelcomeDetails onNext={onBuyerSignup} />}
+          {opened === "buyer" && <WelcomeDetails onNext={onBuyerSignup} />}
 
-          {!toggleDetails && (
+          {(opened == "" || opened === "staff") && (
             <>
               <div
-                className='flex justify-between items-center border border-[#D4C6E8] bg-[#DAB9FA17] bg-opacity-[9%] rounded-[12px] py-3 px-5 cursor-pointer hover:shadow-md hover:shadow-[#8077F626] hover:blur-[4] transition-all duration-300'
-                onClick={onStaffSignup}
+                className="flex justify-between items-center border border-[#D4C6E8] bg-[#DAB9FA17] bg-opacity-[9%] rounded-[12px] py-3 px-5 cursor-pointer hover:shadow-md hover:shadow-[#8077F626] hover:blur-[4] transition-all duration-300"
+                onClick={() => handleOpened("staff")}
               >
-                <div className='flex space-x-1 items-center'>
-                  <Image src={staff} alt='staff' />
-                  <p className='font-satoshi font-medium text-[16px] text-[#090B0C] leading-[20px]'>
+                <div className="flex space-x-1 items-center">
+                  <Image src={staff} alt="staff" />
+                  <p className="font-satoshi font-medium text-[16px] text-[#090B0C] leading-[20px]">
                     Create staff account
                   </p>
                 </div>
-                <Image src={arrowRightPurple} alt='arrow right purple' />
+                {opened === "staff" ? (
+                  <Image src={arrowDownPurple} alt="arrow down purple" />
+                ) : (
+                  <Image src={arrowRightPurple} alt="arrow right purple" />
+                )}
               </div>
-              <div className='flex space-x-1 items-center font-satoshi font-medium text-[16px] leading-[20px] text-[#5D6974] self-end'>
-                <p>Already have an account?</p>
-                <Link href='/login' className='text-[#702EB0]'>
-                  Login
-                </Link>
-              </div>
+              {opened === "staff" && <WelcomeDetails onNext={onStaffSignup} />}
             </>
           )}
+          <div className="flex space-x-1 items-center font-satoshi font-medium text-[16px] leading-[20px] text-[#5D6974] self-end">
+            <p>Already have an account?</p>
+            <Link href="/login" className="text-[#702EB0]">
+              Login
+            </Link>
+          </div>
         </div>
       </div>
     </div>
