b5c4b44c08539acab5eb12b4bfb0eeb2cf514b07
mac <mac@macs-MacBook-Pro.local>
2024-08-29 12:52:31 -0700

    navbar handled for mobile view

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -91,21 +91,21 @@ function Dashboard() {
           toolTip="Whatever the info is for this particular card, usually a short description for the wallet type"
         />
       </div>
-      <div className="mt-10 flex flex-row w-full gap-[10px]">
+      <div className="mt-10 flex flex-row  gap-[10px]">
         <Button
           text="Transfer Points"
           bg="bg-app-purple"
-          classNames="p-3 text-white w-[157px] h-[51px]"
+          classNames="p-3 text-white w-[50%] lg:w-[157px] h-[51px]"
           onClick={() => setTransferIsOpen(true)}
         />
         <Button
           text="Buy Points"
           bg="bg-white"
-          classNames="p-3 text-app-purple border w-[157px] h-[51px]"
+          classNames="p-3 text-app-purple border w-[50%] lg:w-[157px] h-[51px]"
           onClick={() => setBuyIsOpen(true)}
         />
       </div>
-
+      
       <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-4 mt-10 overflow-auto mb-[100px]">
         <div className="border h-[484px] rounded-[6px] p-4 min-w-[500px]">
           <div className="flex items-center justify-between">
@@ -204,108 +204,6 @@ function Dashboard() {
         onClose={() => setTransferIsOpen(false)}
       />
       <BuyPointsModal open={buyIsOpen} onClose={() => setBuyIsOpen(false)} />
-
-      {/* <Modal open={transferIsopen}>
-        <div className="bg-white rounded-lg shadow-lg w-[90%] md:w-[436px] mx-auto">
-          <div className="bg-title-bg-color rounded-tl-lg rounded-tr-lg h-[68px] px-8 flex items-center">
-            <h2 className="font-medium text-[20px] text-black">
-              Transfer Points
-            </h2>
-          </div>
-          <div className="p-8 overflow-auto h-[500px] md:h-full ">
-            <div>
-              <label className="font-satoshi">Select Wallet</label>
-              <div className=" flex flex-col border rounded-lg px-2 mt-2 bg-input-bg">
-                <select className="h-[54px] border-none px-2 outline-none font-satoshi bg-input-bg">
-                  <option>Retail Wallet</option>
-                  <option>Reward Wallet</option>
-                </select>
-              </div>
-            </div>
-            <div className="rounded-lg h-[52px] p-[10px] gap-[10px] border border-[#A855F785] mt-6 flex items-center bg-faint-peach">
-              <FaCircleInfo className="text-app-purple" />
-              <p className="font-satoshi text-[12px] font-[400] text-app-purple">
-                Kindly note that you would be charged 15% on this transfer,
-                learn more
-              </p>
-            </div>
-            <div className="mt-6">
-              <div className="flex flex-row items-center justify-between">
-                <label className="font-satoshi">Enter Amount</label>
-                <div className="flex flex-row items-center justify-between gap-2">
-                  <div className="w-[18px] h-[18px] rounded-[50%] bg-faint-peach2 flex items-center justify-center">
-                    <CiCreditCard1 className="text-[12px]" />
-                  </div>
-                  <p className="font-satoshi text-[12px] font-[400]">
-                    Available DMP: 20000000
-                  </p>
-                </div>
-              </div>
-              <div className=" flex flex-col border rounded-lg px-2 mt-2 bg-input-bg">
-                <input
-                  type="number"
-                  placeholder="10000"
-                  min={200}
-                  className="h-[54px] border-none px-2 outline-none font-satoshi bg-input-bg"
-                />
-              </div>
-            </div>
-            <div>
-              <div className="flex flex-row items-center justify-end gap-2 mt-[12px]">
-                <div className="w-[18px] h-[18px] rounded-[50%] bg-faint-peach2 flex items-center justify-center">
-                  <FaCircleInfo className="text-[12px] text-app-purple" />
-                </div>
-                <p className="font-satoshi text-[12px] font-[400]">
-                  Network Fees: <span className="text-app-purple">1500</span>
-                </p>
-              </div>
-            </div>
-            <div className="mt-6">
-              <div className="flex flex-row items-center justify-between">
-                <label className="font-satoshi">Enter User details</label>
-                <div className="flex flex-row items-center justify-between gap-2">
-                  <p className="font-satoshi text-[12px] text-app-purple font-[400]">
-                    Transfer to Bank Account
-                  </p>
-                </div>
-              </div>
-              <div className=" flex flex-col border rounded-lg px-2 mt-2 bg-input-bg">
-                <input
-                  type="text"
-                  placeholder="Tola Banjo"
-                  className="h-[54px] border-none px-2 outline-none font-satoshi bg-input-bg"
-                />
-              </div>
-            </div>
-            <div className="mt-6">
-              <div className="flex flex-row items-center justify-between">
-                <label className="font-satoshi">Narration</label>
-              </div>
-              <div className=" flex flex-col border rounded-lg px-2 mt-2 bg-input-bg">
-                <input
-                  type="number"
-                  placeholder="Payment for food"
-                  className="h-[54px] border-none px-2 outline-none font-satoshi bg-input-bg"
-                />
-              </div>
-            </div>
-          </div>
-
-          <div className="bg-title-bg-color mt-5 px-8 py-5 flex items-center gap-[10px] justify-end rounded-bl-lg rounded-br-lg">
-            <Button
-              text="Cancel"
-              bg="bg-white"
-              classNames="w-[120px] h-[45px] border border-app-purple text-app-purple"
-              onClick={() => setTransferIsOpen(false)}
-            />
-            <Button
-              text="Cancel"
-              bg="bg-app-purple"
-              classNames="w-[120px] h-[45px] border border-app-purple text-white"
-            />
-          </div>
-        </div>
-      </Modal> */}
     </DashboardLayout>
   );
 }
