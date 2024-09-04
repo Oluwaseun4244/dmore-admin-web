d562c38ab2134c8807eb3a48adc4f130588ccf35
mac <banjotola12@gmail.com>
2024-09-04 01:05:16 -0700

    screen issue

--- src/app/components/generic/Modal.tsx
+++ src/app/components/generic/Modal.tsx
@@ -1,18 +1,37 @@
+// import React, { ReactNode } from "react";
+
+// interface ModalProps {
+//   children: ReactNode;
+//   open: boolean;
+//   //   onClose: (value: boolean)=>void
+// }
+// const Modal: React.FC<ModalProps> = ({ children, open }) => {
+//   return open ? (
+//     <div className="fixed inset-0 bg-gray-800 bg-opacity-75 flex justify-center items-center z-50 overflow-hidden ">
+//       {children}
+//     </div>
+//   ) : (
+//     <></>
+//   );
+// };
+
+// export default Modal;
+
 import React, { ReactNode } from "react";
 
 interface ModalProps {
   children: ReactNode;
   open: boolean;
-  //   onClose: (value: boolean)=>void
 }
+
 const Modal: React.FC<ModalProps> = ({ children, open }) => {
   return open ? (
-    <div className="fixed inset-0 bg-gray-800 bg-opacity-75 flex justify-center items-center z-50 overflow-hidden ">
-      {children}
+    <div className="fixed inset-0 bg-gray-800 bg-opacity-75 flex justify-center items-center z-50 overflow-hidden">
+      {/* <div className="bg-white p-6 max-h-[80vh] w-[90%] md:w-auto rounded overflow-y-auto"> */}
+        {children}
+      {/* </div> */}
     </div>
-  ) : (
-    <></>
-  );
+  ) : null;
 };
 
 export default Modal;
