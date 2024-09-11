cfd0f7dd5089433a222aa11a01c6f5538decc52e
Banjo Tola <banjotola12@gmail.com>
2024-09-11 17:47:59 +0100

    returned layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,7 @@
-import React from "react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import ReactQueryProvider from "./utils/providers/ReactQueryProvider";
 import "./globals.css";
-import { useQueryClient } from '@tanstack/react-query';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,18 +15,6 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const queryClient = useQueryClient();
-
-  // const profileQuery = useGetQuery<ProfileResponse>("/profile", [
-  //   `profile-${'token'}`, token
-  // ]);
-
-  React.useEffect(() => {
-    const token = localStorage.getItem('authToken');
-
-
-  }, [queryClient])
-
   return (
     <html lang="en">
       <body className={`${inter.className} overflow-x-hidden`}>
