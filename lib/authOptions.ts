d6b1e2398ed69d2d4819a010a2d49e05468232c0
mac <banjotola12@gmail.com>
2024-09-20 08:33:04 -0700

    pulling

--- lib/authOptions.ts
+++ lib/authOptions.ts
@@ -12,6 +12,7 @@ interface DecodedToken {
   "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/surname": string;
   "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/mobilephone": string;
   exp: number;
+  exp2: number;
 }
 
 
