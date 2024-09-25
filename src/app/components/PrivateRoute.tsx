b13923f08f9134deef2dc3c8f75fbca4b1f2d950
MICHAEL AJAYI O <ajayimichael@hotmail.com>
2024-09-25 11:18:41 +0100

    add homepage to public routes

--- src/app/components/PrivateRoute.tsx
+++ src/app/components/PrivateRoute.tsx
@@ -18,7 +18,6 @@ const PrivateRoute: React.FC<{ children: React.ReactNode }> = ({
       if (!session) {
         const returnUrl = encodeURIComponent(pathname as string);
         router.push(`/login?returnUrl=${returnUrl}`);
-        // router.push("/login");
       } else {
         setHasSession(true);
       }
@@ -30,18 +29,6 @@ const PrivateRoute: React.FC<{ children: React.ReactNode }> = ({
     }
   }, [status, router, pathname]);
 
-  // useEffect(() => {
-  //   console.log("session", session);
-  //   console.log("status", status);
-  // }, [session, status]);
-
-  // useEffect(() => {
-  //   if (status === "unauthenticated") {
-  //     const returnUrl = encodeURIComponent(pathname as string);
-  //     router.push(`/login?returnUrl=${returnUrl}`);
-  //   }
-  // }, [status, router, pathname]);
-
   if (loading) {
     return (
       <div className='w-screen h-screen flex justify-center items-center'>
@@ -50,51 +37,7 @@ const PrivateRoute: React.FC<{ children: React.ReactNode }> = ({
     );
   }
 
-  // return status === "authenticated" ? <>{children}</> : null;
   return hasSession ? <>{children}</> : null;
 };
 
 export default PrivateRoute;
-
-
-
-// import { useSession, getSession } from "next-auth/react";
-// import { useRouter } from "next/navigation";
-// import { useEffect, useState } from "react";
-
-// const PrivateRoute: React.FC<{ children: React.ReactNode }> = ({
-//   children,
-// }) => {
-//   const { status } = useSession();
-//   const router = useRouter();
-//   const [loading, setLoading] = useState(true);
-//   const [hasSession, setHasSession] = useState(false);
-
-//   useEffect(() => {
-//     const checkSession = async () => {
-//       const session = await getSession();
-//       if (!session) {
-//         router.push("/login");
-//       } else {
-//         setHasSession(true);
-//       }
-//       setLoading(false);
-//     };
-
-//     if (status !== "loading") {
-//       checkSession();
-//     }
-//   }, [status, router]);
-
-//   if (loading) {
-//     return (
-//       <div className='w-screen h-screen flex justify-center items-center'>
-//         <p className='font-sans text-white text-5xl'>Loading...</p>
-//       </div>
-//     );
-//   }
-
-//   return hasSession ? <>{children}</> : null;
-// };
-
-// export default PrivateRoute;
